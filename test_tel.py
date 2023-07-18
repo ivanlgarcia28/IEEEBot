afef98a9dbd39d23124b948651600f6d14c659f0
Ivan <ivanlgarcia28@gmail.com>
2023-07-18 10:07:33 -0300

    modify json and add description

--- test_tel.py
+++ test_tel.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import json
 from config import *
+from telegram.constants import ParseMode
 
 # Put the token that you received from BotFather in the quotes
 bot = Bot(token=TELEGRAM_TOKEN)
@@ -34,48 +35,67 @@ async def check_button(call: types.CallbackQuery):
 		array_of_events = data['events']
 		button_events = []
 		markup = types.InlineKeyboardMarkup()
-
+		caption = "<b>Events list, choose one for more information!</b>"
 		for key, value in array_of_events.items():
 			button_events.append(key)
 			markup.add(InlineKeyboardButton(text=value['name'], callback_data=key))
 
-		await call.message.answer(text='Events' , reply_markup=markup)
+		await call.message.answer(text=caption, parse_mode='HTML' , reply_markup=markup)
 
 		@dp.callback_query_handler(text=button_events)
 		async def eventsdescription_button(call: types.CallbackQuery):
+			await call.message.answer(text=array_of_events[call.data]["name"], parse_mode='HTML')
 			await call.message.answer(text=array_of_events[call.data]["description"])
-
+			await call.message.answer(text=array_of_events[call.data]["url"])
 	elif call.data == "reminders_button":
 		array_of_reminders = data['reminders']
-		cantReminders = len(array_of_reminders)
+		button_reminders = []
 		markup1 = types.InlineKeyboardMarkup()
+		caption1 = "<b>Reminders list, choose one for more information!</b>"
+		for key, value in array_of_reminders.items():
+			button_reminders.append(key)
+			markup1.add(InlineKeyboardButton(text=value['name'], callback_data=key))
 
-		for i in range(cantReminders):
-			reminder = array_of_reminders[i]
-			markup1.add(InlineKeyboardButton(text=reminder['name'], callback_data=f'{reminder["id"]}_button'))
-		await call.message.answer(text='Reminders', reply_markup=markup1)
+		await call.message.answer(text=caption1, parse_mode='HTML', reply_markup=markup1)
+
+		@dp.callback_query_handler(text=button_reminders)
+		async def remindersdescription_button(call: types.CallbackQuery):
+			await call.message.answer(text=array_of_reminders[call.data]["name"])
+			await call.message.answer(text=array_of_reminders[call.data]["description"])
+			await call.message.answer(text=array_of_reminders[call.data]["description"])
 
 	elif call.data == "fundings_button":
 		array_of_fundings = data['fundings']
-		cantFundings = len(array_of_fundings)
+		button_fundings = []
 		markup2 = types.InlineKeyboardMarkup()
+		caption2 = "<b>Fundings list, choose one for more information!</b>"
+		for key, value in array_of_fundings.items():
+			button_fundings.append(key)
+			markup2.add(InlineKeyboardButton(text=value['name'], callback_data=key))
 
-		for i in range(cantFundings):
-			funding = array_of_fundings[i]
-			markup2.add(InlineKeyboardButton(text=funding['name'], callback_data=f'{funding["id"]}_button'))
-		
-		await call.message.answer(text='Fundings', reply_markup=markup2)
+		await call.message.answer(text=caption2, parse_mode='HTML' , reply_markup=markup2)
+
+		@dp.callback_query_handler(text=button_fundings)
+		async def fundingsdescription_button(call: types.CallbackQuery):
+			await call.message.answer(text=array_of_fundings[call.data]["name"])
+			await call.message.answer(text=array_of_fundings[call.data]["description"])
+			await call.message.answer(text=array_of_fundings[call.data]["url"])
 	elif call.data == "resources_button":
 		array_of_resources = data['resources']
-		cantResources = len(array_of_resources)
+		button_resources = []
 		markup3 = types.InlineKeyboardMarkup()
+		caption3 = "<b>Resourcers list, choose one for more information!</b>"
+		for key, value in array_of_resources.items():
+			button_resources.append(key)
+			markup3.add(InlineKeyboardButton(text=value['name'], callback_data=key))
 
-		for i in range(cantResources):
-			resources = array_of_resources[i]
-			markup3.add(InlineKeyboardButton(text=resources['name'], callback_data=f'{resources["id"]}_button'))
-	
-		await call.message.answer(text='Resources', reply_markup=markup3)
-	
+		await call.message.answer(text=caption3, parse_mode='HTML' , reply_markup=markup3)
+
+		@dp.callback_query_handler(text=button_resources)
+		async def resourcesdescription_button(call: types.CallbackQuery):
+			await call.message.answer(text=array_of_resources[call.data]["name"])
+			await call.message.answer(text=array_of_resources[call.data]["description"])
+			await call.message.answer(text=array_of_resources[call.data]["url"])
 	# Notify the Telegram server that the callback query is answered successfully
 	await call.answer()
 	
